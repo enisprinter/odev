69a1ea951aa8bc64a0b1491811add3f3691333ba
Enis Yazıcı <enis-ahemib@outlook.com>
2022-11-23 20:05:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-var zaman = 300
-function gerisayim(){
-if (zaman!=0){
-zaman-=1
-document.getElementById('sayac').innerHTML=zaman
-}
-else{
-window.location.href = "index8.html"; 
-return
-}
-setTimeout("gerisayim()",1000)
-}
-gerisayim()
+// var zaman = 300
+// function gerisayim(){
+// if (zaman!=0){
+// zaman-=1
+// document.getElementById('sayac').innerHTML=zaman
+// }
+// else{
+// window.location.href = "index8.html"; 
+// return
+// }
+// setTimeout("gerisayim()",1000)
+// }
+// gerisayim()
 
+function gerisayim(dakika, saniye) {
+    if(dakika === 0 && saniye === 0) {
+      window.location.href = "index8.html"; 
+      return false;
+    }
+    if(saniye === 0) {
+      saniye = 59;
+      dakika = dakika - 1;
+    } else {
+      saniye = saniye - 1;
+    }
+    
+    let zaman = dakika.toString() + ":" + saniye.toString();
+  
+    document.getElementById("zaman").innerText = zaman; 
+    let t = setTimeout(function(){ gerisayim(dakika, saniye) }, 1000);
+  }
+  
+  gerisayim(0, 10);
